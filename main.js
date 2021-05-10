a6f760c06d0f95c3d162ff860b32e985e4774f7c
Jose Banega <banegajose26@gmail.com>
2021-05-10 20:57:09 -0300

    feat: play with trasnfromations and groups

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 import "./style.css";
 import * as THREE from "three";
-
+const { PI } = Math;
 //* * Canvas
 const canvas = document.querySelector(".webgl");
 //* * Scene
 const scene = new THREE.Scene();
+// Group
 
-// * *Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xd04030 });
-const mesh = new THREE.Mesh(geometry, material);
+const group = new THREE.Group();
 
-mesh.rotation.y = 45;
-mesh.rotation.z = -5;
-mesh.position.z = -1;
-scene.add(mesh);
+// * * Cube A
+
+const geometry1 = new THREE.BoxGeometry(1, 1, 1);
+const material1 = new THREE.MeshBasicMaterial({ color: 0xd04030 });
+const cube1 = new THREE.Mesh(geometry1, material1);
+cube1.position.random();
+// * * Cube B
+const geometry2 = new THREE.BoxGeometry(1, 2, 1);
+const material2 = new THREE.MeshBasicMaterial({ color: 0xd44a385 });
+const cube2 = new THREE.Mesh(geometry2, material2);
+cube2.position.random();
+
+// * * Add group
+
+group.add(cube1);
+group.add(cube2);
+
+// ** Add Scene
+
+scene.add(group);
+
+group.translateX(2);
+group.rotation.x = PI / 2;
+group.rotation.z = PI / 2;
+
+// * * Axes Helper
+
+const axesHelper = new THREE.AxesHelper(10);
+scene.add(axesHelper);
 
 //* * Sizes
 
@@ -25,7 +48,8 @@ const sizes = {
 
 //* * Camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
-camera.position.z = 2;
+camera.position.z = 10;
+camera.position.x = 5;
 scene.add(camera);
 
 //* *Renderer
